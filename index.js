a52b35d00708040ce26c0524418f8cfc1da08445
Sjoerd de Vries <info@supersjoerd.nl>
2021-12-29 12:08:54 +0100

    puppeteer

--- index.js
+++ index.js
@@ -6,27 +6,31 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 const { resolve } = require('path');
+const puppeteer = require('puppeteer');
 
 const fsPromise = fs.promises;
 
 const productUrl = process.env.PRODUCT_URL;
+const tempFile = './current_price.txt';
 const priceSelector = '#corePrice_desktop > div > table > tbody > tr:nth-child(1) > td.a-span12 > span.a-price.a-text-price.a-size-medium.apexPriceToPay > span.a-offscreen';
 
 const botToken = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(botToken);
 
+const _this = this;
+
 const fetchCoffee = () => {
     return new Promise(async (resolve, reject) => {
         try {
-        
-            const response = await axios.get(productUrl);
-            const html = response.data;
-            const $ = cheerio.load(html);
-            const price = $(priceSelector);
-            console.log('Found product with price: '+price.text());
-    
-            resolve(price.text());
-    
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(productUrl);
+            await page.waitForSelector(priceSelector);
+            const textContent = await page.evaluate(() => {
+                return document.querySelector('#corePrice_desktop > div > table > tbody > tr:nth-child(1) > td.a-span12 > span.a-price.a-text-price.a-size-medium.apexPriceToPay > span.a-offscreen').textContent;
+            });
+            browser.close();
+            resolve(textContent);    
         } catch (error) {
             reject(error);
         }
@@ -37,14 +41,14 @@ const isPriceDifferent = (price) => {
     return new Promise(async (resolve, reject) => {
         let previousPrice = '';
         try {
-            previousPrice = await fsPromise.readFile('./current_price.txt', 'utf-8');
+            previousPrice = await fsPromise.readFile(tempFile, 'utf-8');
         } catch (error) {
-            await fsPromise.writeFile('./current_price.txt', '');
+            await fsPromise.writeFile(tempFile, '');
         }
         
         console.log('Previous price: '+previousPrice);
         if (price !== previousPrice) {
-            await fsPromise.writeFile('./current_price.txt', price);
+            await fsPromise.writeFile(tempFile, price);
         }
         resolve(price !== previousPrice);
     });
